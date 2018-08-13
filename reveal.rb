373c62b2443f955b94f7366b51965ed03ad4b674
Benedek Kozma <cyberbeni@gmail.com>
2018-08-13 21:06:44 +0200

    Update reveal.rb

--- reveal.rb
+++ reveal.rb
@@ -1,13 +1,12 @@
 class Reveal < Formula
   desc "Version 13 of Reveal"
-  version '13'
-  sha256 '1cdae698fe0dd3d7d0f457fb572fc063dc006efa0c9a9b59e6beabebb65099ff'
+  sha256 "1cdae698fe0dd3d7d0f457fb572fc063dc006efa0c9a9b59e6beabebb65099ff"
 
-  url "https://download.revealapp.com/Reveal.app-#{version}.zip"
-  name 'Reveal'
-  homepage 'https://revealapp.com/'
+  url "https://download.revealapp.com/Reveal.app-13.zip"
+  name "Reveal"
+  homepage "https://revealapp.com/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  app 'Reveal.app'
+  app "Reveal.app"
 end
