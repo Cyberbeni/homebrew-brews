37be38d1004d3a2bc85bb4d23025d53425b8cb8a
Benedek Kozma <cyberbeni@gmail.com>
2018-08-13 21:41:55 +0200

    Update reveal.rb

--- reveal.rb
+++ reveal.rb
@@ -1,16 +1,9 @@
-
-class Reveal < Formula
-  desc "Version 13"
-  homepage "https://revealapp.com/"
-  url "https://download.revealapp.com/Reveal.app-13.zip"
-  sha256 "1cdae698fe0dd3d7d0f457fb572fc063dc006efa0c9a9b59e6beabebb65099ff"
-  # depends_on "cmake" => :build
-
-  def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
-  end
+cask 'reveal13' do
+  version '13'
+  sha256 '1cdae698fe0dd3d7d0f457fb572fc063dc006efa0c9a9b59e6beabebb65099ff'
+  url "https://download.revealapp.com/Reveal.app-#{version}.zip"
+  name 'Reveal'
+  homepage 'https://revealapp.com/'
+  depends_on macos: '>= :sierra'
+  app 'Reveal.app'
 end
