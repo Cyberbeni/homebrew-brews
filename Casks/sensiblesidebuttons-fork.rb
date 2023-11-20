39175a127aa9a3d5dfe05b4211d0377318e072de
Benedek Kozma <cyberbeni@gmail.com>
2023-11-20 09:11:40 +0100

    Update sensiblesidebuttons-fork.rb to version 1.0.9

--- Casks/sensiblesidebuttons-fork.rb
+++ Casks/sensiblesidebuttons-fork.rb
@@ -1,8 +1,8 @@
 cask 'sensiblesidebuttons-fork' do
-  version "1.0.8"
+  version "1.0.9"
   auto_updates false
   url "https://github.com/Cyberbeni/sensible-side-buttons/archive/refs/tags/#{version}.tar.gz"
-  sha256 "9e9043ad935c477c974362ea3ade20c0fb3d6bb9f62f4db9af03ea61a145eca1"
+  sha256 "783d45c1078fab4284f8d4d3c27fdf9d6e9d1ce933261298d79c839eb60175d0"
   conflicts_with cask: "sensiblesidebuttons"
 
   preflight do
