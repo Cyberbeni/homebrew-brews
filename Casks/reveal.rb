7373d958067b6a63623330ddfeea161313d590ac
Benedek Kozma <benedek.kozma@zenitech.co.uk>
2025-05-19 14:36:35 +0200

    remove unused formulas that also clash with upstream

--- Casks/reveal.rb
+++
@@ -1,9 +0,0 @@
-cask 'reveal' do
-  version '13'
-  sha256 '1cdae698fe0dd3d7d0f457fb572fc063dc006efa0c9a9b59e6beabebb65099ff'
-  url "https://download.revealapp.com/Reveal.app-#{version}.zip"
-  name 'Reveal'
-  homepage 'https://revealapp.com/'
-  depends_on macos: '>= :sierra'
-  app 'Reveal.app'
-end
